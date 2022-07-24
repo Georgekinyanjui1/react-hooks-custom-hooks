8edde99e2787fbd537372c7dd7b52fa086cfc948
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-07-24 22:47:52 +0300

    Done.

---
+++ src/ArticlePage.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import useQuery from "../hooks/useQuery";
+import { makeEmojiList } from "../utils";
+
+function ArticlePage() {
+  // fetch data for a post
+  const { id } = useParams();
+  const{data:post,isLoaded}=useQuery(`http://localhost:4000/posts/${id}`)
+  // set the document title
+  const pageTitle = post ? `Underreacted | ${post.title}` : "Underreacted";
+  useDocumentTitle(pageTitle)
+
+  if (!isLoaded) return <h3>Loading...</h3>;
+
+  const { minutes, title, date, preview } = post;
+  const emojis = makeEmojiList(minutes);
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>
+        {date} • {emojis} {minutes} min read
+      </small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default ArticlePage;
+
---
+++ src/HomePage.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import useQuery from "../hooks/useQuery";
+
+function HomePage() {
+  // fetch data for posts
+  const{data:posts,isLoaded}=useQuery('http://localhost:4000/posts')
+
+  // set the document title
+  
+  useDocumentTitle('underreacted | Home');
+
+  return (
+    <>
+      <About />
+      {isLoaded ? <ArticleList posts={posts} /> : <h3>Loading...</h3>}
+    </>
+  );
+}
+
+export default HomePage;
